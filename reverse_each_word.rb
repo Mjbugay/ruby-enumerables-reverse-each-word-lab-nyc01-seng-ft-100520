40db4348d25c5635c5805cb661937f8e866a1f05
Matt Bugay <mjbugay@yahoo.com>
2020-09-22 17:44:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(string)
   original_array = string.split("")
   test_array = []
-  original_array.each do |
+  original_array.each do |string|
+    test_array << string.reverse
 end
